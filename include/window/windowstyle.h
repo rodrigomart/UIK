ae05e24523fba3f04a264bfa501e10940d7ccb53
Rodrigo Martins <rodrigo.idx@hotmail.com>
2016-03-20 18:38:16 -0300

    Display window style

---
+++ include/window/windowstyle.h
@@ -0,0 +1,15 @@
+#ifndef UIK_WINDOWSTYLE
+#define UIK_WINDOWSTYLE
+
+namespace UIK {
+	namespace WindowStyle {
+		/** Window style */
+		enum {
+			Fullscreen = 1 << 3,
+			Window     = 1 << 2,
+			PopUp      = 1 << 1
+		};
+	}
+}
+
+#endif
