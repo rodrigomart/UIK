cc58cf1202e08a152a43046b3db5019164cb1f7a
Rodrigo Martins <rodrigo.idx@hotmail.com>
2016-03-28 09:42:09 -0300

    Update keyboard.cpp

--- src/input/keyboard.cpp
+++ src/input/keyboard.cpp
@@ -1,9 +1,10 @@
 #include "input/keyboard.h"
+#include "platform/mswin/inputimpl.h"
 
 
 namespace UIK {
 	/** Check if a key is pressed */
 	bool Keyboard::KeyPressed(Key key){
-		return false;
+		return Platform::InputImpl::isKeyPressed(key);
 	}
 }
