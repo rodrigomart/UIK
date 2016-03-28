d9b248cbf784afe4c37ee7f20e2a728da82f57e8
Rodrigo Martins <rodrigo.idx@hotmail.com>
2016-03-28 01:00:44 -0300

    Update uik.h

--- include/uik.h
+++ include/uik.h
@@ -1,10 +1,15 @@
 #ifndef UIK_
 #define UIK_
 
+#include "point.h"
+
+// WINDOW
 #include "window/window.h"
 #include "window/windowstyle.h"
 
+// INPUTS
 #include "input/mouse.h"
 #include "input/keyboard.h"
 
 #endif
+
